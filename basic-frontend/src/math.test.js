6c4a88fd86c6a65cfa9642458c780d487bd974aa
Dan Moffat <danmoffat@gmail.com>
2024-06-01 22:15:22 +0100

    Added a second test

--- basic-frontend/src/math.test.js
+++ basic-frontend/src/math.test.js
@@ -4,3 +4,7 @@ import { add } from './math';
 it('adds 1 + 2 to equal 3', () => {
   expect(add([1, 2])).toBe(3);
 });
+
+it('evauluates all elements of an array of unknown length', () => {
+  expect(add([1, 2, 3, 4, 5])).toBe(15);
+});
