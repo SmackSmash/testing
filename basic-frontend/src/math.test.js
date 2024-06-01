164f3fa519914fe0ece7b6bf3ca9a2421ace3ea1
Dan Moffat <danmoffat@gmail.com>
2024-06-01 22:20:48 +0100

    Added more tests

--- basic-frontend/src/math.test.js
+++ basic-frontend/src/math.test.js
@@ -8,3 +8,11 @@ it('adds 1 + 2 to equal 3', () => {
 it('evauluates all elements of an array of unknown length', () => {
   expect(add([1, 2, 3, 4, 5])).toBe(15);
 });
+
+it('returns 0 for an empty array', () => {
+  expect(add([])).toBe(0);
+});
+
+it('works with negative numbers', () => {
+  expect(add([-1, -2, -3])).toBe(-6);
+});
